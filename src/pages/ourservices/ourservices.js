423e86f97ba520691300925bb708dcbc1839cdb7
Deepthi9745 <deepthim9745@gmail.com>
2025-03-14 16:51:16 +0530

    Updated responsive styles for 1133px screen width

--- src/pages/ourservices/ourservices.js
+++ src/pages/ourservices/ourservices.js
@@ -95,9 +95,9 @@ const OurServices = () => {
             <p className="highlighted-text4">
                 WHY DO YOU NEED AN INSURANCE BROKER?
             </p>
-
+            
             <p className="highlighted-text4-content">The world of insurance is complex and everchanging. The insurance covers provided by the policy you bought last year may no longer be suitable or sufficient for your needs. You may not know that the quoted premiums are competitive? And if you are just starting out or expanding your business, how do you know what types of insurance you need? And if the insurance you buy is good enough to protect your business against the risk exposures? A well experienced and suitably qualified professional can help assess and source the types and levels of cover that you need, saving you the trouble and effort. The consequences of not having the right insurance in place can be disastrous, and sometimes this only comes to light when you need to make a claim, which might be too late.</p>
-
+           
 
             <div className="business-container">
                 <h2 className="business-title">WE UNDERSTAND YOUR BUSINESS</h2>
